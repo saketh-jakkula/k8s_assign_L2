7dfc45f9288285135e8e99e80a8e811e7c3bcc49
Saketh Jakkula <43716650+saketh-linux@users.noreply.github.com>
2019-09-22 08:28:15 +0530

    Update wrapper_l2.sh

--- wrapper_l2.sh
+++ wrapper_l2.sh
@@ -164,8 +164,8 @@ alias gssh='gcloud compute ssh --project "$project" --zone "$zone"'
 project=$1
 zone=$2
 
-gcloud compute addresses create k8smaster --ip-version IPV4 --region $zone
-address=`gcloud compute addresses list|awk '{print $2}'|grep [0-9]`
+gcloud compute addresses create k8smaster  --region $zone
+address=`gcloud compute addresses list|awk '{print $2}'|grep -v [a-z/A-Z]`
 
 gcloud beta compute --project=$project instances create instance-1 --zone=$zone --machine-type=n2-standard-2 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=298530575220-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/cloud-platform --image=centos-7-v20190905 --image-project=centos-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=instance-1 --reservation-affinity=any --address=$address
 
